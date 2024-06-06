d39ba41c0e11084fa889f1b1564c5cbefaede30e
VIKRAM067 <vvikram0678@gmail.com>
2024-06-06 09:43:41 +0530

    feat: modified

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ require('dotenv').config();
 const express = require('express');
 const ExpressLayouts = require('express-ejs-layouts');
 
+const dbconnect = require('./server/config/dbConnection');
+
+dbconnect();
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
 
+app.use(express.static('public'));
+
 // templating the ejs:
 app.use(ExpressLayouts);
-app.set('layouts','./layouts/main');
+
 app.set('view engine','ejs');
+app.set('layout','./layouts/main');
 
 app.use('/', require('./server/routes/main'));
+
 
 app.listen(PORT,()=>{
     console.log(`app is listening on PORT: ${PORT}`);
